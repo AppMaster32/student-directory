b11a951c3385a4b313e947738eef6c112713a675
Appmaster <appmaster32@hotmail.com>
2014-08-07 15:25:09 +0100

    centerd

--- directory.rb
+++ directory.rb
@@ -1,27 +1,58 @@
 @students = []
 
-def print_header
-  puts "The students of my cohort at Makers Academy"
-  puts "-------------"
+def fl_txt
+  3.times do
+  print "\r#{ msg }"
+  sleep 0.5
+  print "\r#{ ' ' * msg.size }" # Send return and however many spaces are needed.
+  sleep 0.5
 end
+end
+
+
+def welcome
+  puts " "
+puts " "
+    fl_txt.msg3 = "Welcome to student directory!".center(80)
+  
+
+print "Welcome to student directory".center(100)
+
+end
+
+
+def print_header
+
+  
+  p
+  print 'The students of my cohort at Makers Academy'.center(100)
+  puts '-------------------------------------------'.center(100)
+  
+end
+
 
 def print_students_list
   @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(100)
   end
 end
 
-def print_footer    
-  puts "Overall, we have #{@students.length} great students"
+def print_footer  
+t = Time.new
+print "#{t.strftime ("As of %Y-%m")}, we have#{@students.length} great students".center(100)
+ print "=======================================================".center(100)
+p     
+p          
 end
 
 def add_student(name, cohort)
-    @students << {:name => name.capitalize, :cohort => cohort}    
+    @students << {:name => name, :cohort => cohort}    
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  p
+  p "Please enter the names of the students"
+  p "To finish, just hit return twice"
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
@@ -34,35 +65,53 @@ def input_students
   end
 end
 
-def print_menu    
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit" # 9 because we'll be adding more items  
+def print_menu  
+
+ p 
+p
+  puts "1. Input the students ".center(100)
+  puts "2. Show the students".center(99)
+  puts "3. Save the list to students.csv".center(110)
+  puts "4. Load the list from students.csv".center(112)
+  puts "9. Exit".center(85) # 9 because we'll be adding more items  
+  puts ""
 end
 
 def show_students
-  print_header
-  print_students_list
-  print_footer
+  print_header.center(100)
+  print_students_list.center(100)
+  print_footerc.center(100)
 end
 
 
-def load_students
+def load_students(filename = "students.csv")
   file = File.open("students.csv", "r")
   file.readlines.each do |line| 
     name, cohort = line.chomp.split(',')
     add_student(name, cohort)
+
+    puts ""
   end
   file.close
-  puts "The names have been loaded from students.csv"
-  puts""
+  puts ""
+       msg = 'Loading from students.csv'
+
+  3.times do
+  print "\r#{ msg }"
+  sleep 0.5
+  print "\r#{ ' ' * msg.size }" # Send return and however many spaces are needed.
+  sleep 0.5
+end
+  puts"Loaded from students.csv"
+  puts ""
+  show_students
+  p 
+
 end
 
 
 def save_students
-	file = File.open("students.csv","w")
+	file = File.open("students.csv", "w")
 
 	@students.each do |student|
 		student_data = [student[:name], student[:cohort]]
@@ -70,7 +119,18 @@ def save_students
 		file.puts csv_line
 	end
 	file.close
-  puts "The names have been saved to students.csv"
+  p 
+     msg = 'Saving to students.csv'
+
+3.times do
+  print "\r#{ msg }"
+  sleep 0.5
+  print "\r#{ ' ' * msg.size }" # Send return and however many spaces are needed.
+  sleep 0.5
+end
+  puts "Saved to students.csv"
+  p
+  p
 end
 	
 
@@ -90,17 +150,42 @@ when "4"
 
 
   when "9"
-    exit
+exit
   else
-    puts "I don't know what you mean, try again"
+    msg = 'I dont know what you mean, try again!!'
+
+3.times do
+  print "\r#{ msg }"
+  sleep 0.5
+  print "\r#{ ' ' * msg.size }" # Send return and however many spaces are needed.
+  sleep 0.5
+end
+    print "I dont know what you mean, try again!!"
+    puts ""
+    puts ""
+  end
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.length} from #{filename}"
+  else
+
+  puts "Sorry, #{filename} dosen't exist!!!!"
+
+  exit 
   end
 end
 
 def interactive_menu  
   loop do
     print_menu        
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
-
+welcome
+try_load_students
 interactive_menu
